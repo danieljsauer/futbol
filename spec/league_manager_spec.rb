26b969d77fd156d7cf58dbed185ba46eb14d4668
Allen Russell <garyrusselljr@gmail.com>
2023-06-09 16:32:01 -0600

    test: Add test for exists and count of teams

---
+++ spec/league_manager_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe LeagueManager do
+  before(:each) do
+    game_path = './fixtures/games.csv'
+    team_path = './fixtures/teams.csv'
+    game_teams_path = './fixtures/game_teams.csv'
+    locations = {
+                games: game_path,
+                teams: team_path,
+                game_teams: game_teams_path
+                }
+    stat_tracker = StatTracker.new(locations)
+    @league_manager = LeagueManager.new(game_path, stat_tracker)
+  end
+
+  describe '#exists' do
+    it 'can initialize' do
+      expect(@league_manager).to be_a(LeagueManager)
+    end
+  end
+
+  describe '#count_of_teams' do
+    it 'can count total number of teams in the data' do
+      expect(@league_manager.count_of_teams).to be_a(Integer)
+      expect(@league_manager.count_of_teams).to eq(32)
+    end
+  end
+end
