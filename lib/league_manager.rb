eb1f8b13a22eff547a2b6a594e2cab3605c8de8b
Allen Russell <garyrusselljr@gmail.com>
2023-06-09 16:46:28 -0600

    feat: Add count_of_teams method

--- lib/league_manager.rb
+++ lib/league_manager.rb
@@ -1,21 +1,21 @@
 class LeagueManager
-  attr_reader :game_path,
+  attr_reader :team_path,
               :stat_tracker
 
-  def initialize(game_path, stat_tracker)
+  def initialize(team_path, stat_tracker)
     @league = []
     @stat_tracker = stat_tracker
-    create_leagues(game_path)
+    create_leagues(team_path)
   end
 
-  def create_leagues(game_path)
-    data = CSV.parse(File.read(game_path), headers: true, header_converters: :symbol)
+  def create_leagues(team_path)
+    data = CSV.parse(File.read(team_path), headers: true, header_converters: :symbol)
     @league = data.map do |leag|
       League.new(leag)
     end
   end
 
   def count_of_teams
-    
+    @league.count
   end
 end
