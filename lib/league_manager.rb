fc72e26dbcd8abf710be4ed8a1ff27a1363bd545
Allen Russell <garyrusselljr@gmail.com>
2023-06-09 16:33:15 -0600

    feat: Add initialize, and create_leagues methods

--- lib/league_manager.rb
+++ lib/league_manager.rb
@@ -1,9 +1,21 @@
-require_relative 'Futbol'
+class LeagueManager
+  attr_reader :game_path,
+              :stat_tracker
 
-class LeagueManager < Futbol
-  attr_reader :
+  def initialize(game_path, stat_tracker)
+    @league = []
+    @stat_tracker = stat_tracker
+    create_leagues(game_path)
+  end
 
-  def initialize(locations)
-    super(locations)
+  def create_leagues(game_path)
+    data = CSV.parse(File.read(game_path), headers: true, header_converters: :symbol)
+    @league = data.map do |leag|
+      League.new(leag)
+    end
+  end
+
+  def count_of_teams
+    
   end
 end
