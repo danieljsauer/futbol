1dd38af82d873c73b4e1c199723f769a9134ec5e
Justin Pasko <jpasko1112@gmail.com>
2023-06-10 12:55:39 -0400

    fix: removed unnecessary data.

--- lib/season_manager.rb
+++ lib/season_manager.rb
@@ -1,9 +1,19 @@
-require_relative 'Futbol'
+class SeasonManager
+  attr_reader :season_path,
+              :stat_tracker
 
-class SeasonManager < Futbol
-
-
-  def initialize(locations)
-    super(locations)
+  def initialize(season_path, stat_tracker)
+    @seasons = []
+    @stat_tracker = stat_tracker
+    create_leagues(season_path)
   end
+
+  def create_leagues(season_path)
+    data = CSV.parse(File.read(season_path), headers: true, header_converters: :symbol)
+    @seasons = data.map do |seas|
+      Season.new(seas)
+    end
+  end
+
+  
 end
