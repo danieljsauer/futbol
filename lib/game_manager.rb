d98106ba591dac039d2531390282215d93644c36
Allen Russell <garyrusselljr@gmail.com>
2023-06-09 19:53:32 -0600

    feat: Add total_games_per_team, total_scores_per_team, games_played, and average_goals_per_game methods

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -27,4 +27,30 @@ class GameManager
       game.total_score
     end.total_score
   end
+
+  def total_games_per_team
+    @games.each_with_object(Hash.new(0)) do |game, hash|
+      hash[game.home_team_id] += 1
+      hash[game.away_team_id] += 1
+    end
+  end
+
+  def total_scores_per_team
+    @games.each_with_object(Hash.new(0)) do |game, hash|
+      hash[game.home_team_id] += game.home_goals
+      hash[game.home_team_id] += game.away_goals
+    end
+  end
+
+  def games_played(team_id)
+    total_games_per_team[team_id]
+  end
+
+  def average_goals_per_game
+    hash = Hash.new
+    total_scores_per_team.each do |team_id, goals|
+    hash[team_id] = goals.to_f / games_played(team_id)
+    end
+    hash
+  end
 end
