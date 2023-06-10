6afcfca27d51537383d54dd86f698b91907b5c25
Allen Russell <garyrusselljr@gmail.com>
2023-06-09 19:55:01 -0600

    feat: Add most_wins_team_id and most_losses_team_id helper methods

--- lib/league_manager.rb
+++ lib/league_manager.rb
@@ -1,12 +1,10 @@
 class LeagueManager
   attr_reader :team_path,
-              :stat_tracker,
-              :games
+              :stat_tracker
 
-  def initialize(team_path, stat_tracker, games)
+  def initialize(team_path, stat_tracker)
     @league = []
     @stat_tracker = stat_tracker
-    @games = games
     create_leagues(team_path)
   end
 
@@ -22,52 +20,19 @@ class LeagueManager
   end
 
   def best_offense
-    # key is team id, value is score
-    total_scores_per_team = Hash.new(0)
-    # key is team id, value is number of games
-    total_games_per_team = Hash.new(0)
-    
-    @games.each do |game|
-      total_scores_per_team[game.home_team_id] += game.home_goals
-      total_scores_per_team[game.home_team_id] += game.away_goals
-      
-      total_games_per_team[game.home_team_id] += 1
-      total_games_per_team[game.away_team_id] += 1
-    end
-
-    average_goals_per_game = Hash.new
-    total_scores_per_team.each do |id, goals|
-      games_played = total_games_per_team[id]
-      average_goals_per_game[id] = goals.to_f / games_played
-    end
-
-    most_wins_team_id = average_goals_per_game.max_by { |id, goals| goals }[0]
-    best_team = @league.find { |team| team.team_id == most_wins_team_id }
-    best_team.team_name
+    @league.find { |team| team.team_id == most_wins_team_id }.team_name
   end
 
   def worst_offense
-    # key is team id, value is score
-    total_scores_per_team = Hash.new(0)
-    # key is team id, value is number of games
-    total_games = Hash.new(0)
+    @league.find { |team| team.team_id == most_losses_team_id }.team_name
+  end
 
-    @games.each do |game|
-      total_scores_per_team[game.home_team_id] += game.home_goals
-      total_scores_per_team[game.home_team_id] += game.away_goals
+  # helper methods
+  def most_wins_team_id
+    @stat_tracker.average_goals_per_game.max_by { |id, goals| goals }[0]
+  end
 
-      total_games[game.home_team_id] += 1
-      total_games[game.away_team_id] += 1
-    end
-
-    average_goals_per_game = Hash.new
-    total_scores_per_team.each do |id, goals|
-      games_played = total_games[id]
-      average_goals_per_game[id] = goals.to_f / games_played
-    end
-
-    most_losses_team_id = average_goals_per_game.min_by { |id, goals| goals }[0]
-    worst_team = @league.find { |team| team.team_id == most_losses_team_id }
-    worst_team.team_name
+  def most_losses_team_id
+    @stat_tracker.average_goals_per_game.min_by { |id, goals| goals }[0]
   end
 end
