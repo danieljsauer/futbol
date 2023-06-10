202aa4b33a8f521c3f5857c020390a3c234f0fc1
Allen Russell <garyrusselljr@gmail.com>
2023-06-09 18:04:11 -0600

    feat: Add best_offense method

--- lib/league_manager.rb
+++ lib/league_manager.rb
@@ -1,10 +1,12 @@
 class LeagueManager
   attr_reader :team_path,
-              :stat_tracker
+              :stat_tracker,
+              :games
 
-  def initialize(team_path, stat_tracker)
+  def initialize(team_path, stat_tracker, games)
     @league = []
     @stat_tracker = stat_tracker
+    @games = games
     create_leagues(team_path)
   end
 
@@ -17,5 +19,30 @@ class LeagueManager
 
   def count_of_teams
     @league.count
+  end
+
+  def best_offense
+    # key is team id, value is score
+    total_scores = Hash.new(0)
+    # key is team id, value is number of games
+    total_games = Hash.new(0)
+
+    @games.each do |game|
+      total_scores[game.home_team_id] += game.home_goals
+      total_scores[game.home_team_id] += game.away_goals
+
+      total_games[game.home_team_id] += 1
+      total_games[game.away_team_id] += 1
+    end
+
+    average_goals_per_game = Hash.new
+    total_scores.each do |id, goals|
+      games_played = total_games[id]
+      average_goals_per_game[id] = goals.to_f / games_played
+    end
+
+    most_wins_team_id = average_goals_per_game.max_by { |id, goals| goals }[0]
+    best_team = @league.find { |team| team.team_id == most_wins_team_id }
+    best_team.team_name
   end
 end
