57f9e54aec34584d0ab90c7e28a27bd5b170ca87
Justin Pasko <jpasko1112@gmail.com>
2023-06-10 12:46:54 -0400

    test: Add tests for highest_total_score, lowest_total_score, percentage_home_wins, percentage_visitor_wins, percentage_ties and all helper method tests.

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -34,6 +34,24 @@ RSpec.describe GameManager do
     end
   end
 
+  describe '#percentage_home_wins' do
+    it 'should return the percentage of home wins' do
+      expect(@game_manager.percentage_home_wins).to eq(0.72)
+    end
+  end
+
+  describe '#percentage_visitor_wins' do
+    it 'should return the percentage of visitor wins' do
+      expect(@game_manager.percentage_visitor_wins).to eq(0.24)
+    end
+  end
+
+  describe '#percentage_ties' do
+    it 'should return the percentage of ties' do
+      expect(@game_manager.percentage_ties).to eq(0.03)
+    end
+  end
+
   describe '#total_games_per_team' do
     it 'can find the total number of games per team' do
       expected = {6=>9, 3=>5, 5=>4, 16=>12, 17=>7, 8=>5, 9=>5, 30=>5, 19=>3, 26=>3}
@@ -56,8 +74,28 @@ RSpec.describe GameManager do
 
   describe '#average_goals_per_game' do
     it 'can give the average goals per game for the season' do
-      expected = {6=>2.0, 3=>1.6, 5=>2.0, 16=>2.0, 17=>1.5714285714285714, 8=>2.6, 9=>2.0, 30=>1.6, 19=>2.0, 26=>0.3333333333333333}
+      expected = {6=>2.0, 3=>1.6, 5=>2.0, 16=>2.0, 17=>1.57, 8=>2.6, 9=>2.0, 30=>1.6, 19=>2.0, 26=>0.33}
       expect(@game_manager.average_goals_per_game).to eq(expected)
+    end
+  end
+  
+  # ------Helper Method Tests-------
+
+  describe '#total_games' do
+    it 'returns the total number of games' do
+      expect(@game_manager.total_games).to eq(29)
+    end
+  end
+
+  describe '#home_wins' do
+    it 'retruns a count of the total number of home team wins' do
+      expect(@game_manager.home_wins).to eq(21.0)
+    end
+  end
+
+  describe '#game_ties' do
+    it 'retruns a count of the total number of ties' do
+      expect(@game_manager.game_ties).to eq(1.0)
     end
   end
 end
