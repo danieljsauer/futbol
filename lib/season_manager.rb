4a5d1eb297628bfe0bd172f430c1ceb5ef9622d6
Justin Pasko <jpasko1112@gmail.com>
2023-06-11 11:58:59 -0400

    Add psuedo code for first method.

--- lib/season_manager.rb
+++ lib/season_manager.rb
@@ -9,14 +9,20 @@ class SeasonManager
     create_leagues(season_path)
   end
 
-  def create_leagues(season_path)
+  def create_managers(season_path)
     data = CSV.parse(File.read(season_path), headers: true, header_converters: :symbol)
     @seasons = data.map do |seas|
       Season.new(seas)
     end
   end
 
-  def best_coach
-    
+  def best_coach(season)
+    require 'pry'; binding.pry
+    # this method will take an argument of season.
+    # we will then need to iterate through each season and find the team with the most wins.
+    # we will also need to iterate through a season with a team id and find total games played by each team.
+    # we will then need to divide total wins by each team by total games by each team.
+    # we will return the team id with the highest percentage.
+    # we will then take that team id and match it with the coach of that team.
   end
 end
