f2ca8e32e6d70b6ee2faac72a65667aedd9c5a9e
Allen Russell <garyrusselljr@gmail.com>
2023-06-11 08:30:07 -0600

    fix: Convert strings to integers per running SimpleCov

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,14 +3,12 @@ require 'spec_helper'
 RSpec.describe Game do
   before(:each) do
     @game = Game.new(
-                    game_id: '9',
-                    season: '12',
-                    away_team_id: '14',
-                    home_team_id: '23',
-                    away_goals: '0',
-                    home_goals: '3',
-                    home_team_name: 'Home Team',
-                    away_team_name: 'away'
+                    game_id: 9,
+                    season: 12,
+                    away_team_id: 14,
+                    home_team_id: 23,
+                    away_goals: 0,
+                    home_goals: 3
                   )
   end
 
@@ -20,14 +18,12 @@ RSpec.describe Game do
     end
 
     it 'has attributes' do
-      expect(@game.game_id).to eq('9')
-      expect(@game.season).to eq('12')
-      expect(@game.away_team_id).to eq('14')
-      expect(@game.home_team_id).to eq('23')
-      expect(@game.away_goals).to eq('0')
-      expect(@game.home_goals).to eq('3')
-      expect(@game.home_team_name).to eq('Home Team')
-      expect(@game.away_team_name).to eq('away')
+      expect(@game.game_id).to eq(9)
+      expect(@game.season).to eq(12)
+      expect(@game.away_team_id).to eq(14)
+      expect(@game.home_team_id).to eq(23)
+      expect(@game.away_goals).to eq(0)
+      expect(@game.home_goals).to eq(3)
     end
   end
 end
--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe League do
   before(:each) do
     @league = League.new(
-                        team_id: '8',
+                        team_id: 8,
                         teamname:'AFC Richmond'
                         )
   end
@@ -14,7 +14,7 @@ RSpec.describe League do
     end
 
     it 'has readable attributes' do
-      expect(@league.team_id).to eq('8')
+      expect(@league.team_id).to eq(8)
       expect(@league.team_name).to eq('AFC Richmond')
     end
   end
